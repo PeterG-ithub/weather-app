218c018a068fa67883917d6ce85b6a8c94f70627
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-10-10 11:24:58 -0700

    add getWeatherInfo function

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 console.log("I AM MAIN.JS");
 
+const search = document.querySelector(".search");
+
 addEventListener("submit", (event) => {
   console.log("Submit button has been clicked");
   event.preventDefault();
+  //console.log(search.value);
+  getWeatherInfo(search.value);
 });
+
+async function getWeatherInfo(name) {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=867872936e964f17b47174912231010&q=${name}`
+  ).then(function (response) {
+    console.log(response.json());
+  });
+}
