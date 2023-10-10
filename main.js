99d32e595d3f0d4f23d2fe8e4162bc155a1e3a52
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-10-10 11:47:18 -0700

    add getLocationsInfo function
    This function displays all the locations with the same
    city name for the q= parameter

--- main.js
+++ main.js
@@ -6,13 +6,31 @@ addEventListener("submit", (event) => {
   console.log("Submit button has been clicked");
   event.preventDefault();
   //console.log(search.value);
-  getWeatherInfo(search.value);
+  getLocationsInfo(search.value);
 });
+
+async function getLocationsInfo(name) {
+  fetch(
+    `https://api.weatherapi.com/v1/search.json?key=867872936e964f17b47174912231010&q=${name}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      response.forEach((element) => {
+        getWeatherInfo(element.name);
+      });
+    });
+}
 
 async function getWeatherInfo(name) {
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=867872936e964f17b47174912231010&q=${name}`
-  ).then(function (response) {
-    console.log(response.json());
-  });
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.location.name);
+    });
 }
