0eb20ede3aa8e28b069cfd9e89c99074793e537d
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-10-10 12:32:46 -0700

    add displayWeatherInfo function
    this function will create a node for each location
    that have similar names to the search results using template
    string with a predetermined style on style.css

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 console.log("I AM MAIN.JS");
 
 const search = document.querySelector(".search");
-
+const preview = document.querySelector(".preview-container");
 addEventListener("submit", (event) => {
   console.log("Submit button has been clicked");
   event.preventDefault();
   //console.log(search.value);
+  deleteOldLocation();
   getLocationsInfo(search.value);
 });
+
+function deleteOldLocation() {
+  preview.innerHTML = "";
+}
 
 async function getLocationsInfo(name) {
   fetch(
@@ -18,19 +23,34 @@ async function getLocationsInfo(name) {
     })
     .then(function (response) {
       response.forEach((element) => {
-        getWeatherInfo(element.name);
+        // console.log(`${element.name}, ${element.region}, ${element.country}`);
+        getWeatherInfo(
+          `${element.name}, ${element.region}, ${element.country}`
+        );
       });
     });
 }
 
 async function getWeatherInfo(name) {
   fetch(
-    `https://api.weatherapi.com/v1/current.json?key=867872936e964f17b47174912231010&q=${name}`
+    `https://api.weatherapi.com/v1/forecast.json?key=867872936e964f17b47174912231010&q=${name}`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      console.log(response.location.name);
+      displayWeatherInfo(response);
     });
+}
+
+function displayWeatherInfo(response) {
+  console.log(response);
+  const item = document.createElement("div");
+  item.innerHTML = `<div class="item-container">
+    <div class="item-head"> ${response.location.name}, ${response.location.region}, ${response.location.country}, ${response.location.localtime}</div>
+    <div class="temperature">Feels like ${response.current.feelslike_c} celsius</div>
+
+    <button class="select-button">Select</button>
+  </div>`;
+  preview.appendChild(item);
 }
